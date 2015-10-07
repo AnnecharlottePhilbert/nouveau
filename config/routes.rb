979a8581af21d427d134837903413efdfb690935
AnnecharlottePhilbert <acphilbert@hotmail.com>
2015-10-07 19:25:17 +0200

    commit après book workshop 3 partie 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :shows
   get 'home/index'
 
-  get 'home/index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
